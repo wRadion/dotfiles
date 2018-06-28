fbef4f2d1c3d98932239690513af453c82afa704
wRadion <wradion@gmail.com>
2018-06-29 01:13:48 +0200

    Update font.sh

--- src/custom/font.sh
+++ src/custom/font.sh
@@ -3,7 +3,7 @@
 # Check for yaourt installation
 pacman -Q yaourt
 if [ $? = 0 ]; then
-  sudo yaourt -S ttf-dejavu-sans-mono-powerline-git
+  yaourt -S ttf-dejavu-sans-mono-powerline-git
 else
   echo "You must install yaourt before installing the font"
 fi
